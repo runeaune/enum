80103cfaac82c4394873be81166166f4b9b5da09
Rune T. Aune <rune@hakksw.com>
2024-09-12 15:19:15 +0300

    add 'space' to list of allowed input formats

--- cmd/go-enum/main.go
+++ cmd/go-enum/main.go
@@ -13,7 +13,7 @@ func main() {
 		fileName   = kingpin.Arg("input-file", "Uses $GOFILE if that is set.").Envar("GOFILE").Required().ExistingFile()
 		lineNum    = kingpin.Flag("line", "Location of const statement to generate enum from. Uses $GOLINE if that is set.").Default("1").Envar("GOLINE").Int()
 		trimPrefix = kingpin.Flag("trim", "Prefix to trim from the enum type name when generating the strings.").Default("").String()
-		formatFunc = kingpin.Flag("format", "How to format string value").Default("snake").Enum("snake", "camel", "upper", "lower", "first", "first-upper", "first-lower", "capitalize-first", "capitalize-all")
+		formatFunc = kingpin.Flag("format", "How to format string value").Default("snake").Enum("space", "snake", "camel", "upper", "lower", "first", "first-upper", "first-lower", "capitalize-first", "capitalize-all")
 		json       = kingpin.Flag("json", "Generate code implementing (un)marshal interface").Default("true").Bool()
 		value      = kingpin.Flag("with-value", "Generate code implementing Value() to allow the actual value").Default("false").Bool()
 	)
